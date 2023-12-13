e337a8594cb768f1071e0fa3129e70ee73d536d6
Maelys Dumergue <madumerg@3F3.42angouleme.fr>
2023-12-13 12:54:20 +0100

    fdf

--- mlx_window.c
+++ mlx_window.c
@@ -6,27 +6,13 @@
 /*   By: madumerg <madumerg@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 14:45:09 by madumerg          #+#    #+#             */
-/*   Updated: 2023/12/12 12:41:29 by madumerg         ###   ########.fr       */
+/*   Updated: 2023/12/13 12:49:11 by madumerg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 # define WINDOW_WIDTH 1000
 # define WINDOW_HEIGHT 700
-
-
-int	main(void)
-{
-	void	*mlx_ptr;
-    void	*win_ptr;
-
-    mlx_ptr = mlx_init();	
-    win_ptr = mlx_new_window(mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "My first window!");
-	mlx_loop(mlx_ptr);
-    mlx_destroy_window(mlx_ptr, win_ptr);
-    mlx_destroy_display(mlx_ptr);
-    free(mlx_ptr);
-}
 
 int		ft_count_line(int	fd)
 {
@@ -70,21 +56,36 @@ char	**ft_read_map(char *fd, int size)
 char	**ft_parsing_map(char **map)
 {
 	int	i;
+	int	j;
 	char	**temp;
-	t_map t;
+	t_map **parsed_map;
 
 	i = 0;
 	while (map[i])
 	{
-		temp += ft_split(map, 32);
-		while (temp[i])
+		j = 0;
+		temp[i] = ft_split(map[i], 32);
+		while (temp[j])
 		{
-			temp [i] = ft_atoi(map[i]);
-			i++;
+			map[j] = ft_atoi(temp[j]);
+			j++;
 		}
 		i++;
 	}
-	return (map);
+	parsed_map = ft_calloc(sizeof(char), i);
+	if (!parsed_map)
+		return (NULL);
+	ft_map_color(parsed_map, map);
+	return (parsed_map);
+}
+
+char	ft_map_height_color(t_map *parsed_map, char **map)
+{
+	while (parsed_map->y < map)
+	{
+
+	}
+	0x00FF0000;
 }
 
 char	ft_print_line(int x, int y)
@@ -105,6 +106,15 @@ char	ft_print_line(int x, int y)
 	}
 }
 
-ft_split(map);
-ft_itoa(file);
+int	main(void)
+{
+	void	*mlx_ptr;
+    void	*win_ptr;
 
+    mlx_ptr = mlx_init();	
+    win_ptr = mlx_new_window(mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "My first window!");
+	mlx_loop(mlx_ptr);
+    mlx_destroy_window(mlx_ptr, win_ptr);
+    mlx_destroy_display(mlx_ptr);
+    free(mlx_ptr);
+}
